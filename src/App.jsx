18f6ee7cf64d74fccde0d8d25e4f6a718057f545
Mabaidul-Hoque <mabaidulhoque95@gmail.com>
2024-09-02 22:36:42 +0530

    adding branches feature into newBranch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Branches from "./components/Branches";
 
 function App() {
   return (
     <div>
       <h1>Git and Github Tuitorial</h1>
       <p>This is a tutorial on how to use Git and Github</p>
+
+      <Branches />
     </div>
   );
 }
